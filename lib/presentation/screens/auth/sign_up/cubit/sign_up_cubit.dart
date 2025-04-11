123582f0cec5d9d5596c492fdae821b3156f2b5a
OskarPerliński <oskar.perlinskii@gmail.com>
2025-04-11 13:00:05 +0200

    add sign-up functionality; implement SignUpCubit and SignUpAction with error handling

---
+++ lib/presentation/screens/auth/sign_up/cubit/sign_up_cubit.dart
@@ -0,0 +1,40 @@
+import 'package:hooked_bloc/hooked_bloc.dart';
+import 'package:injectable/injectable.dart';
+import 'package:trailer_hub/domain/auth/error/auth_exception.dart';
+import 'package:trailer_hub/domain/auth/repository/auth_repository.dart';
+import 'package:trailer_hub/presentation/screens/auth/sign_up/cubit/sign_up_action.dart';
+import 'package:trailer_hub/presentation/screens/auth/sign_up/cubit/sign_up_state.dart';
+
+@injectable
+class SignUpCubit extends ActionCubit<SignUpState, SignUpAction> {
+  SignUpCubit(this.authRepository) : super(SignUpState.initial());
+
+  final AuthRepository authRepository;
+
+  Future<void> signUpWithEmail(
+    String email,
+    String password,
+    String confirmPassword,
+  ) async {
+    if (password != confirmPassword) {
+      emit(SignUpInitial(errorMessage: 'Passwords do not match'));
+      return;
+    }
+
+    dispatch(const SignUpAction.showLoading());
+    try {
+      await authRepository.signUpWithEmail(email, password);
+      dispatch(const SignUpAction.success());
+    } on AuthException catch (e) {
+      emit(SignUpInitial(errorMessage: e.message));
+    } catch (e) {
+      dispatch(
+        SignUpAction.showErrorMessage(
+          'An error occurred while trying to register.',
+        ),
+      );
+    } finally {
+      dispatch(SignUpAction.hideLoading());
+    }
+  }
+}
