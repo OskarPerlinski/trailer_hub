4ed700228a0414b7ea03fc92a6035d0c2ef464ee
OskarPerliński <oskar.perlinskii@gmail.com>
2025-04-12 08:03:27 +0200

    feat: add SignInCubit and actions for user authentication flow

---
+++ lib/presentation/screens/auth/sign_in/cubit/sign_in_cubit.dart
@@ -0,0 +1,31 @@
+import 'package:hooked_bloc/hooked_bloc.dart';
+import 'package:injectable/injectable.dart';
+import 'package:trailer_hub/domain/auth/error/auth_exception.dart';
+import 'package:trailer_hub/domain/auth/repository/auth_repository.dart';
+import 'package:trailer_hub/presentation/screens/auth/sign_in/cubit/sign_in_action.dart';
+import 'package:trailer_hub/presentation/screens/auth/sign_in/cubit/sign_in_state.dart';
+
+@injectable
+class SignInCubit extends ActionCubit<SignInState, SignInAction> {
+  SignInCubit(this.authRepository) : super(SignInState.initial());
+
+  final AuthRepository authRepository;
+
+  Future<void> signInWithEmail(String email, String password) async {
+    dispatch(const SignInAction.showLoading());
+    try {
+      await authRepository.signInWithEmail(email, password);
+      dispatch(SignInAction.success());
+    } on AuthException catch (e) {
+      emit(SignInInitial(errorMessage: e.message));
+    } catch (e) {
+      dispatch(
+        SignInAction.showErrorMessage(
+          'An error occurred while trying to login.',
+        ),
+      );
+    } finally {
+      dispatch(SignInAction.hideLoading());
+    }
+  }
+}
