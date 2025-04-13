e36ed74a7fb856af4e6a0bf3bc037b01277db1e1
OskarPerliński <oskar.perlinskii@gmail.com>
2025-04-13 09:59:17 +0200

    feat: add injectable annotation to GoogleDataSource for dependency injection

--- lib/data/auth/datasource/google_data_source.dart
+++ lib/data/auth/datasource/google_data_source.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:injectable/injectable.dart';
 
+@singleton
 class GoogleDataSource {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
